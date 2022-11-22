cc396258a37f7f970a8e3063aea38e4246e7b423
Alexandria <alexandriacwhite@comcast.net>
2022-11-21 22:14:08 -0600

    Hobbies index is console.log selected items only now - Thank you Justin!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 var badRequestUrl = './index.html';
 var redirectUrl = './404.html';
+var user = localStorage.getItem('blueOceanUser') || {};
+
+
 
 
 /*404 error
@@ -23,11 +26,11 @@ function userInfo (){
 
 
 function hobbiesElement(hobbie) {
-var hobbieElement =`<div>
+    var hobbieElement = `<div>
 <input type="checkbox" class="hobbieInput" name=${hobbie} value=${hobbie} />
 <label for=${hobbie}>${hobbie}</label>
 </div>`;
-return hobbieElement;
+    return hobbieElement;
 }
 
 
@@ -37,30 +40,53 @@ const otherText = document.querySelector('#otherValue');
 otherText.style.visibility = 'hidden';
 
 otherCheckbox.addEventListener('change', () => {
-  if (otherCheckbox.checked) {
-    otherText.style.visibility = 'visible';
-    otherText.value = '';
-  } else {
-    otherText.style.visibility = 'hidden';
-  }
+    if (otherCheckbox.checked) {
+        otherText.style.visibility = 'visible';
+        otherText.value = '';
+    } else {
+        otherText.style.visibility = 'hidden';
+    }
 });
 
 
-function appendHobbies(){
-    var hobbies =["swimming",
-    "hiking",
-    "football",
-    "baseball",
-    "soccer",
-    "hockey"];
-    for (var i=0; i <= hobbies.length;i++){
+function appendHobbies() {
+    var hobbies = ["swimming",
+        "hiking",
+        "football",
+        "baseball",
+        "soccer",
+        "hockey"];
+    for (var i = 0; i <= hobbies.length; i++) {
         $("#hobbiesList").append(hobbiesElement(hobbies[i]))
     }
 }
 appendHobbies();
 
 /*Hobbies submit button*/
-$("#hobbiesForm").submit(e=>{
-e.preventDefault();
-console.log("helloWorld");
+$("#hobbiesForm").submit(e => {
+    e.preventDefault();
+    console.log("helloWorld");
+    /*Find all checkboxes in form field*/
+    /*Add all checkboxes with selected property to an array*/
+    //Save array to user profile
+    var userHobbies = [];
+    $("input:checked").each(function() {
+        console.log($(this).val())
+        // TODO:  userHobbies.push($(this).attr('name'));
+    });
+    // TODO: save hobbie array to user object using dot notation
+    console.log({user});
 });
+
+//Example from weather app 
+// function setLocalStorage(city) {
+//     if (localStorage.getItem(searchKey) !== null){
+//     let cities = localStorage.getItem(searchKey)
+//     let citiesSearches = JSON.parse(cities)
+//     citiesSearches.push(city);
+//     localStorage.setItem(searchKey, JSON.stringify(citiesSearches));
+//     } else {
+//     let searches = []
+//     searches.push(city);
+//     localStorage.setItem(searchKey, JSON.stringify(searches));
+// }}
