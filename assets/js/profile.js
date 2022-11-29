62d4ef11eb4fdbd25044806fef49179c2b464a5e
Alexandria <alexandriacwhite@comcast.net>
2022-11-28 21:01:40 -0600

    Added user profile to local storage. Still need to add hobbies and display data

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,3 +1,4 @@
+let profileSaveBtn = document.querySelector(".saveProfile")
 var script = document.createElement('script');
 script.type = 'text/javascript';
 script.src = 'https://geolocation-db.com/json/geoip.php?jsonp=callback';
@@ -9,3 +10,19 @@ function callback(data) {
     state.innerHTML = data.state;
     city.innerHTML = data.city;
 }
+
+// Need to add hobbies in here 
+// Check if hobbies exist, if they do - set new item 
+// Appending user profile to local storage 
+$(".saveProfile").click(()=> {
+    let userProfile = {
+        pic: $("#file").val(),
+        name: $(".name").val(),
+        city: $("#city").val(),
+        state: $("#state").val(),
+        bio: $(".bio").val(),
+        job: $(".job").val(),
+        school: $(".school").val(),
+    }
+    localStorage.setItem("blueOceanUserProfile", JSON.stringify(userProfile));
+})
