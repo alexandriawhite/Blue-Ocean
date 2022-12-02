93a07580ba1e3e696903c0ead4944b181b41ab8d
Alexandria White <114960634+alexandriawhite@users.noreply.github.com>
2022-12-02 11:47:06 -0600

    Updated photo function. Works if you fill out the full form and save then go back to settings

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,42 +1,53 @@
 //Profile picture function
-const loadFile = function (event) {
-  const image = document.getElementById("image_input");
-  image_input.addEventListener("change", function() {
-      const reader = new FileReader();
-      reader.addEventListener("load", () => {
-        const uploaded_image = reader.result;
-        localStorage.setItem("img", JSON.stringify(reader.result))
-        let img = localStorage.getItem("img")
-        let photo = JSON.parse(img)
-        console.log(reader.results)
-       let png = document.querySelector("#image_input") 
-        png.src= reader.results//`url(${uploaded_image})`;
-      });
-      reader.readAsDataURL(this.files[0]);
+// const loadFile = function (event) {
+//   const image = document.getElementById("image_input");
+//   image_input.addEventListener("change", function() {
+//       const reader = new FileReader();
+//       reader.addEventListener("load", () => {
+//         const uploaded_image = reader.result;
+//         localStorage.setItem("img", JSON.stringify(reader.result))
+//         let img = localStorage.getItem("img")
+//         let photo = JSON.parse(img)
+//         console.log(reader.results)
+//        let png = document.querySelector("#image_input") 
+//         png.src= reader.results//`url(${uploaded_image})`;
+//       });
+//       reader.readAsDataURL(this.files[0]);
     
-    });
-  //image.src = URL.createObjectURL(event.target.files[0]);
-}
+//     });
+//   //image.src = URL.createObjectURL(event.target.files[0]);
+// }
+
+
+const image_input = document.querySelector(".load");
+image_input.addEventListener("change", function() {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => {
+    const uploaded_image = reader.result;
+    localStorage.setItem("img", JSON.stringify(reader.result))
+    let img = localStorage.getItem("img")
+    let photo = JSON.parse(img)
+  });
+  reader.readAsDataURL(this.files[0]);
+});
 
 function loadImage(){
   let img = localStorage.getItem("img")
   let photo = JSON.parse(img)
   let image_input = document.getElementById("image_input")
   image_input.src= photo
-  }
-  
-  loadImage();
+}
 
-// const image_input = document.querySelector("#image-input");
-// image_input.addEventListener("change", function() {
-//   const reader = new FileReader();
-//   reader.addEventListener("load", () => {
-//     const uploaded_image = reader.result;
-//     localStorage.setItem("img", JSON.stringify(reader.result))
-//     let img = localStorage.getItem("img")
-//     let photo = JSON.parse(img)
-//     console.log(photo)
-//     document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`;
-//   });
-//   reader.readAsDataURL(this.files[0]);
-// });
+
+  if (localStorage.getItem("img") !== null) {
+    loadImage();
+} 
+
+
+    
+
+
+    
+
+    
+
